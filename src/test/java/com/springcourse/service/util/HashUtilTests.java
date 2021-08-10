d6b35d7d2eb606e353a0a6472c2be6446fb85f4f
Lucas Peres <lucas.peres@ufv.br>
2021-08-10 18:44:26 -0300

    Services

---
+++ src/test/java/com/springcourse/service/util/HashUtilTests.java
@@ -0,0 +1,16 @@
+package com.springcourse.service.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class HashUtilTests {
+	@Test
+
+	public void getSecureHashTest() {
+		String hash = HashUtil.getSecureHash("123");
+		System.out.println(hash);
+		assertThat(hash.length()).isEqualTo(64);
+	}
+}
