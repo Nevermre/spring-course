cfbd575007c61272dad5b95cb711e001c0b8e61b
Lucas Peres <lucas.peres@ufv.br>
2021-08-29 14:01:42 -0300

    password encoder

---
+++ src/main/java/com/springcourse/security/CustomPasswordEncoder.java
@@ -0,0 +1,29 @@
+package com.springcourse.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.springcourse.service.util.HashUtil;
+
+@Component
+public class CustomPasswordEncoder implements PasswordEncoder{
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		String hash = HashUtil.getSecureHash(rawPassword.toString());
+		return hash;
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		// TODO Auto-generated method stub
+		
+		String hash = HashUtil.getSecureHash(rawPassword.toString());
+		
+		
+		return hash.equals(encodedPassword);
+	}
+
+	
+	
+}
